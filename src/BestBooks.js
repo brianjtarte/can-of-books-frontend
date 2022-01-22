f4c77a2f1c41fd901c687131b6ad1d88ed436087
brianjtarte <brianjtarte@gmail.com>
2022-01-22 09:55:32 -0800

    fixed carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -16,44 +16,51 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
-
-
-getBooks = async () => {
-    let apiUrl = `${SERVER}/books`;
-    console.log(apiUrl);
-    const response = await axios.get(apiUrl);
-    this.setState({books: response.data});
-    console.log(response.data[0].title);
-     bookArr = this.state.books.map((book, idx) => {
-      return <Carousel.Item><h2>{this.state.books.title} </h2>
-     <Carousel.Caption><p>{this.state.books.description}</p></Carousel.Caption></Carousel.Item>;
-     
-   });
-  }
-
-
   componentDidMount() {
     this.getBooks();
     console.log('HELLO WORLD!')
   }
 
 
+  getBooks = async () => {
+    let apiUrl = `${SERVER}/books`;
+    console.log(apiUrl);
+    const response = await axios.get(apiUrl);
+    this.setState({ books: response.data });
+    console.log(response.data[0].title);
+
+  }
+
+
+
+
   render() {
     console.log('books');
-    
+
     // /* TODO: render user's books in a Carousel */
-   
+
     console.log(this.state.books[0]);
-    
+
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
         {this.state.books.length ? (
           <Carousel>
-           {bookArr} 
-        </Carousel>
+            {this.state.books.map((book, idx) => (
+              <Carousel.Item key={idx}>
+                  <img src="https://via.placeholder.com/150" alt="coming soon"/>
+                <Carousel.Caption>
+                  <h2>{book.title} </h2>
+                  <p>{book.description}</p>
+                </Carousel.Caption>
+
+              </Carousel.Item>
+
+            ))}
+
+          </Carousel>
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>No Books Found </h3>
         )}
       </>
     )
